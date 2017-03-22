3eb1ca4d87165bc574423685cf9d2bbfce68d500
pranavj1001 <pranavj1001@gmail.com>
2017-03-22 23:07:05 +0530

    update actions.php

--- actions.php
+++ actions.php
@@ -1,16 +1,6 @@
 <?php
 
-    //to start a session
-    session_start();
-    
-    //link variable to store the db address and its details 
-    $link = mysqli_connect("localhost", "root", "", "slackclone");
-    
-    //if connection to db failed
-    if(mysqli_connect_errno()){
-        print_r(mysqli_connect_error());
-        exit();
-    }
+    include("controller.php");
 
     //loginSignup action
     if($_GET['actions'] == "loginSignup"){
