40b989a98f59c83203698513fff4fa8517d72923
pranavj1001 <pranavj1001@gmail.com>
2017-03-27 21:44:28 +0530

    fix the issue and add the new db

--- db/slackclone.sql
+++ db/slackclone.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 23, 2017 at 06:12 AM
+-- Generation Time: Mar 27, 2017 at 06:13 PM
 -- Server version: 10.1.16-MariaDB
 -- PHP Version: 7.0.9
 
@@ -39,7 +39,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `email`, `password`) VALUES
-(0, 'pranav@pranav.com', '28150b466b7cf0ca24f483023f16a41d');
+(1, 'pranav@pranav.com', '28150b466b7cf0ca24f483023f16a41d'),
+(2, 'user1@user1.com', '703d464fe2c3221745b87785d4a8ac5a');
 
 --
 -- Indexes for dumped tables
@@ -51,6 +52,15 @@ INSERT INTO `users` (`id`, `email`, `password`) VALUES
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
