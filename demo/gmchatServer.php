5cddb3c15fb0f9cd69b2d23a27a23899870e9fc0
Ganesh D Choudhary <ganeshmukesh123@gmail.com>
2017-04-06 00:55:39 +0530

    demo

--- demo/gmchatServer.php
+++ demo/gmchatServer.php
@@ -1,8 +1,8 @@
-<?php
+ <?php
 	include("actions.php"); 
   	include("services/loadProfile.php");
 
-  	$lastchayid=$_POST['lastchatid'];
+  	$lastchatid=$_POST['lastchatid'];
 
   	if(isset($_POST['message'])){
 
@@ -12,9 +12,35 @@
   	$row=mysqli_fetch_assoc($result);
   	$TeamName=$row['teamname'];
 
-  	$insert="INSERT INTO '".$TeamName."'()VALUES()";
+  	$insert="INSERT INTO '".$TeamName."'(`chatid`,`message`,`sender`)VALUES('".$_POST['lastchatid']."','".$_POST['message']."','".$_POST['sender']."')";
+
+    $insert_result = mysqli_query($link,$insert);
+}
+
 
   	}
+    $select="SELECT *
+            FROM '".$TeamName."'
+            WHERE chat_id > '".$lastchatid."'
+            ";
+
+    $result = mysqli_query($link,$select);
+    $arr =array();
+    $row_count=mysqli_num_rows($result);
+
+
+    if($row_count>0){
+      while($row=mysqli_fetch_array($result)){
+        array_push($arr, $row);
+      }
+    }
+
+
+    //close mysqli connection
+    mysqli_close($con);
+
+    //return the response as JSON
+    echo json_encode($arr);
 
 
 
