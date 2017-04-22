6c3f613b0d325853c67b1c97ff59bd5634e8ed8d
pranavj1001 <pranavj1001@gmail.com>
2017-04-22 22:24:28 +0530

    add service for defining a new project

--- js/botServices.js
+++ js/botServices.js
@@ -5,9 +5,11 @@ function findTheServiceRequired(message){
 	//console.log(" Reached here" + message);
 
 	if((message.toLowerCase().indexOf("what's the time") >= 0) || (message.toLowerCase().indexOf("whats the time") >= 0) || (message.toLowerCase().indexOf("what is the time") >= 0) || (message.toLowerCase().indexOf("what's the time?") >= 0) || (message.toLowerCase().indexOf("whats the time?") >= 0) || (message.toLowerCase().indexOf("what is the time?") >= 0)){
-		console.log(" Reached here" + message);
+		//console.log(" Reached here" + message);
 		botMessage = "Current Time is ";
 		botMessage += botAction.getTime();
+	}else if((message.toLowerCase().indexOf("define a new project") >= 0)){
+		console.log("Create a new Project");
 	}else{
 		botMessage = "Hey there, I'm a bot.";
 	}
