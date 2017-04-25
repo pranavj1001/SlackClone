9ca510e62a07beeda595e727eb3e5cb8297c9b28
Pranav Jain <pranavj1001@gmail.com>
2017-04-25 11:43:01 +0530

    add logic for defining a new project

--- js/botServices.js
+++ js/botServices.js
@@ -1,4 +1,5 @@
 var botMessage = "Hey there, I'm a bot.";
+var projectDefineSteps = 0;
 
 function findTheServiceRequired(message){
 
@@ -10,6 +11,11 @@ function findTheServiceRequired(message){
 		botMessage += botAction.getTime();
 	}else if((message.toLowerCase().indexOf("define a new project") >= 0)){
 		console.log("Create a new Project");
+		botMessage = "Alright, what's the name of the project?";
+		projectDefineSteps = 1;
+	}else if(projectDefineSteps){
+		botAction.defineANewProject();
+		projectDefineSteps = 0;
 	}else{
 		botMessage = "Hey there, I'm a bot.";
 	}
@@ -20,6 +26,10 @@ var botAction = {
 
 	getTime: function(){ 
 		return new Date().toLocaleTimeString();
+	}
+	
+	defineANewProject: function(){
+		//ajax call to create a new table for each project
 	}
 
 };
