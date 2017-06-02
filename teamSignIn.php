e2c4b173bc8dcf9ebf186c49ec421f250486b412
pranavj1001 <pranavj1001@gmail.com>
2017-06-02 23:15:46 +0530

    add navbar to teamSignIn.php

--- teamSignIn.php
+++ teamSignIn.php
@@ -25,7 +25,29 @@
 
   <body>
     <?php if(isset($_SESSION['id'])){ ?> 
-      <div class="container">
+
+      <nav class="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse">
+        <button class="navbar-toggler navbar-toggler-right hidden-lg-up" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand title">Team Sign In</a>
+
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="http://localhost/SlackClone/dash.php">DashBoard</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Help</a>
+            </li>
+          </ul>
+          <div class="form-inline mt-2 mt-md-0">
+            <a class="btn btn-outline-danger my-2 my-sm-0" href="?function=teamLogout">Donate to Project</a>
+          </div>
+        </div>
+      </nav>
+
+      <div class="container bodyForm">
 
         <div class="alert alert-danger" id="alert"></div>
 
